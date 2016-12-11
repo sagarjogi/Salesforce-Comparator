156b6a2be229731e8acf77ad16d0c6afcf78eb07
sagarjogi <sagarjogi7@gmail.com>
2016-12-11 07:49:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.get("/process_get", function (req, res) {
    res.end(JSON.stringify(response));
 });
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+//var server = app.listen(8081, function () {
+ //  var host = server.address().address
+ //  var port = server.address().port
+  // console.log("Example app listening at http://%s:%s", host, port)
 
-});
+//});
