8874377e0743cf860239cb80c0e36e4e129b808e
sagarjogi <sagarjogi7@gmail.com>
2016-12-11 09:59:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get("/process_get", function (req, res) {
 
 //});
 
-var server = app.listen(process.env.PORT || 3000, function(){
+var server = app.listen(process.env.PORT || 5000, function(){
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
